b7943519e7423dec48f0cc1a82e7042176dc752f
Francisco Gutierrez <fco.gutierrezmonge@gmail.com>
2023-04-05 12:28:35 +0200

    spring Railway.app app config

---
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //Mostramos variables de entorno
+        System.getenv().forEach(
+                (key,value) -> System.out.println(key + " " + value)
+        );
+    }
+
+}
